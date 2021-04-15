430d304b68d0005fc1cb5897aa78590bbb7c5d17
Julie <julienonnenmacher@hotmail.fr>
2021-04-15 18:33:09 +0200

    done

--- index.js
+++ index.js
@@ -21,7 +21,41 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create(
+      {
+        // TODO: write the schema
+        title: "Kimchi Pancakes",
+        level: "Easy Peasy",
+        ingredients: ["kimchi", "flour", "scallions", "sugar","water","vegetable oil"],
+        cuisine: "Korean",
+        dishType: "other",
+        image: "https://www.koreanbapsang.com/wp-content/uploads/2010/02/DSC0780-2.jpg",
+        duration: 15,
+        creator: "Chief Maria and Chief Julie",
+      }
+    )
+
+  })
+  .then((singleTitle) =>{
+    console.log(singleTitle.title)
+    return Recipe.insertMany(data)
+  })
+  .then((allRecipes)=>{
+  allRecipes.forEach(recipe => {
+    console.log(recipe.title)
+    })
+    return Recipe.findOneAndUpdate({title :  "Rigatoni alla Genovese"}, {duration:100})
+  })
+  .then(()=>{
+    console.log('Yay duration is done')
+    return Recipe.deleteOne({title:"Carrot Cake"})
+  })
+  .then(()=>{
+    console.log('The carrot cake is dead! Long live the carrot cake!!')
+    mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+ 
+
